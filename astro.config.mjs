81ac25bdd6d193a7aaba6e72d1a455c071c05e4b
mikel.diez <mikel.diez@somorrostro.com>
2025-04-03 11:36:08 +0200

    vercel bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
 import icon from 'astro-icon'
-import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,8 +16,6 @@ export default defineConfig({
     }),
     compress(),
   ],
-  output: 'server',
-  adapter: vercelServerless(),
   vite: {
     css: {
       preprocessorOptions: {
