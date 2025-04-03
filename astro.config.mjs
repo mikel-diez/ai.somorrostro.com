b2dfdbdb9da17941731388cf881d402ce9c5b412
mikel.diez <mikel.diez@somorrostro.com>
2025-04-03 09:00:32 +0200

    added vercel conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
 import icon from 'astro-icon'
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+
   compressHTML: true,
   site: 'https://mikel-diez.github.io',
   base: '/ai.somorrostro.com',
@@ -17,6 +19,8 @@ export default defineConfig({
     }),
     compress(),
   ],
+  output: 'server',
+  adapter: vercelServerless(),
   vite: {
     css: {
       preprocessorOptions: {
